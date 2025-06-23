d76bc8545c5ed4b85eec20fa4b2d3afc3b9971f1
GorInKot <dann1lg0rin@gmail.com>
2025-06-23 11:20:48 +0300

    Добавлено: Таблица с динамическим добавлением данных из полей, заглушка для табельного номера

--- app/src/main/java/com/example/epi/Fragments/Control/ControlFragment.kt
+++ app/src/main/java/com/example/epi/Fragments/Control/ControlFragment.kt
@@ -3,11 +3,16 @@ package com.example.epi.Fragments.Control
 import android.app.Activity.RESULT_OK
 import android.content.Intent
 import android.content.Intent.ACTION_OPEN_DOCUMENT
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TableLayout
+import android.widget.TableRow
+import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
@@ -35,23 +40,83 @@ class ControlFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         val currentDate = SimpleDateFormat("dd.MM.yyyy", Locale.getDefault()).format(Date())
-//        val currentTime = SimpleDateFormat("HH:mm", Locale.getDefault()).format(Date())
         binding.CtrFrTvDate.text = "Дата: $currentDate"
 
-
-        binding.CtrFrrBtnBack.setOnClickListener {
-            findNavController().navigate(R.id.transportFragment)
+        // Получить номер предписания
+        var counter = 1
+        binding.btnOrderNumber.setOnClickListener {
+            val orderNumber = "1234_$counter"
+            binding.tvOrderNumber.text = orderNumber
+            counter++
         }
 
-        binding.CtrFrBtnReport.setOnClickListener {
-            findNavController().navigate(R.id.inspectionReportFragment)
+
+        // Добавить вид работ
+        binding.btnAddRow.setOnClickListener {
+            val inputEquipmentName = binding.InputEditTextEquipmentName.text.toString().trim()
+            val inputType = binding.InputEditTextType.text.toString().trim()
+            val inputReport = binding.InputEditTextReport.text.toString().trim()
+            val inputRemarks = binding.InputEditTextRemarks.text.toString().trim()
+            val tvOrderNumber = binding.tvOrderNumber.text.toString().trim()
+
+            if (inputEquipmentName.isBlank() || inputType.isBlank() || tvOrderNumber.isBlank() || inputReport.isBlank() || inputRemarks.isBlank()) {
+                Toast.makeText(requireContext(), "Заполните все поля", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            val tableRow = TableRow(requireContext())
+            tableRow.layoutParams = TableLayout.LayoutParams(
+                TableLayout.LayoutParams.MATCH_PARENT,
+                TableLayout.LayoutParams.WRAP_CONTENT
+            )
+
+            fun createCell(text: String): TextView {
+                return TextView(requireContext()).apply {
+                    this.text = text
+                    setPadding(8, 8, 8, 8)
+                    textSize = 18f
+                    setTextColor(Color.BLACK)
+                    gravity = Gravity.CENTER
+                    layoutParams = TableRow.LayoutParams(0, TableRow.LayoutParams.WRAP_CONTENT, 1f)
+                }
+            }
+            Log.d(
+                "TransportFragment",
+                "Прибор: '$inputEquipmentName', " +
+                        "Вид работ: '$inputType'," +
+                        "Номер предписания: '$tvOrderNumber'," +
+                        "Отчет о результатах: '$inputReport'," +
+                        "Замечания: '$inputRemarks'"
+            )
+
+//            Toast.makeText(requireContext(), "input1: $inputEquipmentName", Toast.LENGTH_SHORT).show()
+
+            // Добавляем ячейки в строку
+            tableRow.addView(createCell(inputEquipmentName))
+            tableRow.addView(createCell(inputType))
+            tableRow.addView(createCell(tvOrderNumber))
+            tableRow.addView(createCell(inputReport))
+            tableRow.addView(createCell(inputRemarks))
+
+
+            Log.d("Table", "Добавление данных в таблицу")
+
+            // Очищаем поля ввода
+            binding.InputEditTextEquipmentName.setText("")
+            binding.InputEditTextType.setText("")
+            binding.InputEditTextReport.setText("")
+            binding.InputEditTextRemarks.setText("")
+
+            // Добавляем строку в таблицу
+            binding.table.addView(tableRow)
         }
 
-        binding.CtrFrBtnDocsReport.setOnClickListener {
-            findNavController().navigate(R.id.commentsDocsFragment)
+        // Кнопка "Назад"
+        binding.btnBack.setOnClickListener {
+            findNavController().popBackStack()
         }
-
-        binding.CtrFrrBtnNext.setOnClickListener {
+        // Кнопка "Далее"
+        binding.btnNext.setOnClickListener {
             findNavController().navigate(R.id.fixFragment)
         }
 
